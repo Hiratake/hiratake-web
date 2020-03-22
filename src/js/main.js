d1a4ba3a05597afda5dbdf81363856df2ca56c36
Hiratake <hiratake@hiratake.xyz>
2020-03-22 14:44:25 +0900

    Update: LazyLoad導入

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,16 @@
 // Main.js
 // ================================================================================
 
-import Vue from 'vue';
-import App from '../vue/App.vue';
+import Vue          from 'vue';
+import App          from '../vue/App.vue';
+import VueLazyLoad  from 'vue-lazyload';
+
+Vue.use( VueLazyLoad, {
+  preLoad: 1.2,
+  loading: '/img/loading.gif',
+  attempt: 1,
+  throttleWait: 100
+});
 
 new Vue({
   el: '#app',
