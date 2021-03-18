fdd6f6d8ccabb86de1da84a65147b79172068728
Hiratake <hiratake@hiratake.xyz>
2021-03-18 12:30:48 +0900

    リセット用CSSを読み込み

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import '~/assets/css/reset.css'
 
 export default function (Vue, { router, head, isClient }) {
-  // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 
   head.htmlAttrs = { lang: 'ja' }
