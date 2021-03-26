b004472b5175e3160845d686fbb4307b93ad54b7
Hiratake <hiratake@hiratake.xyz>
2021-03-26 23:44:33 +0900

    カスタムプロパティ用の共通CSSを追加

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+
 import '~/assets/css/reset.css'
+import '~/assets/css/variables.css'
 
 export default function (Vue, { router, head, isClient }) {
   Vue.component('Layout', DefaultLayout)
