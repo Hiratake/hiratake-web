f4f08fe7851c5ce3c376d9e74feb0fc795365cd1
Hiratake <hiratake@hiratake.xyz>
2021-06-07 00:23:57 +0900

    feat: ogp画像を生成してhead内に追加するよう変更

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -4,10 +4,26 @@
 
 <script>
 export default {
-  async asyncData ({ params, error, $content }) {
+  async asyncData ({ params, error, $content, $cloudinary }) {
     const { slug } = params
     try {
       const post = await $content('blog', slug).fetch()
+      post.image = await $cloudinary.image.url(
+        'post_uakfpm.png',
+        {
+          overlay: {
+            font_family: 'notosansjp.otf',
+            font_size: 56,
+            text: encodeURI(post.title),
+          },
+          color: '#394147',
+          width: 1000,
+          x: 128,
+          y: 136,
+          gravity: 'north_west',
+          crop: 'fit',
+        },
+      )
       return { post }
     }
     catch (e) {
@@ -16,8 +32,21 @@ export default {
   },
 
   head () {
+    const post = this.post
+    const meta = []
+    meta.push({
+      hid: 'og:image',
+      property: 'og:image',
+      content: post.image,
+    })
+    meta.push({
+      hid: 'twitter:card',
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    })
     return {
-      title: this.post.title,
+      title: post.title,
+      meta,
     }
   },
 }
