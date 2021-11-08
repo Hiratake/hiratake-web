452f2733263f4d84c64a526a1faa00537317929c
Hiratake <hiratake@hiratake.xyz>
2021-11-09 06:52:35 +0900

    build: nodev16への対応

--- nuxt.config.js
+++ nuxt.config.js
@@ -134,9 +134,6 @@ export default {
     loaders: {
       scss: {
         implementation: require('sass'),
-        sassOptions: {
-          fiber: require('fibers'),
-        },
       },
     },
   },
