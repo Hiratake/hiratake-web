19e9fa19254a564587c475d75f0bd12f38529dff
Hiratake <hiratake@hiratake.xyz>
2021-11-21 00:50:02 +0900

    chore: head内のseo用のタグなどを調整

--- next.config.js
+++ next.config.js
@@ -3,12 +3,20 @@
 const path = require('path')
 
 const baseName = process.env.BASE_NAME || 'Hiratake Web'
+const baseUrl = process.env.BASE_URL || 'https://hiratake.xyz'
 const baseDir = process.env.BASE_DIR || '/'
+const baseImage = process.env.BASE_IMAGE || '/images/default.jpg'
+const baseDescription =
+  process.env.BASE_DESCRIPTION ||
+  'マークアップ園児でデザイナーなひらたけのサイト。'
 
 module.exports = {
   env: {
     baseName,
+    baseUrl,
     baseDir,
+    baseImage,
+    baseDescription,
   },
   images: {
     domains: ['gravatar.com'],
