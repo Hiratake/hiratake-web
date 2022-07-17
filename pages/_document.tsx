59cf8d2623f8b4bdb20dd00f2005614d8bebcb6a
Hiratake <hiratake@hiratake.xyz>
2022-07-18 00:55:03 +0900

    chore: faviconを追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,48 @@
 // pages > _document
 
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { config } from '@/utils/config'
 
 class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="ja">
         <Head>
+          <meta name="apple-mobile-web-app-title" content={config.name} />
+          <meta name="application-name" content={config.name} />
+          <meta
+            name="msapplication-TileColor"
+            content={config.colors.primary}
+          />
+          <meta
+            name="msapplication-config"
+            content="/favicons/browserconfig.xml"
+          />
+          <meta name="theme-color" content={config.colors.primary} />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicons/safari-pinned-tab.svg"
+            color={config.colors.primary}
+          />
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&amp;display=swap"
             rel="stylesheet"
