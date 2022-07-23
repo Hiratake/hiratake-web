5c960b419bf11b6b04b4824bf51e6ba884cb9a38
Hiratake <hiratake@hiratake.xyz>
2022-07-24 00:24:05 +0900

    feat: ボタンに読み込み中表示にする機能を追加

--- components/AppButton.tsx
+++ components/AppButton.tsx
@@ -2,6 +2,7 @@
 
 import { ElementType, MouseEventHandler, ReactNode } from 'react'
 import { css } from '@emotion/react'
+import { AppProgress } from '@/components/AppProgress'
 import { rem } from '@/utils/style'
 
 // ----------------------------------------
@@ -16,6 +17,7 @@ export type AppButtonProps = {
   size: 'large' | 'medium' | 'small'
   outlined?: boolean
   icon?: boolean
+  loading?: boolean
   onClick?: MouseEventHandler<HTMLElement>
 }
 
@@ -33,7 +35,10 @@ export const AppButton = (props: AppButtonProps) => {
       aria-label={props.ariaLabel}
       onClick={props.onClick}
     >
-      <span css={contentStyle}>{props.children}</span>
+      <span css={contentStyle(!!props.loading)}>{props.children}</span>
+      <span css={loaderStyle(!!props.loading)}>
+        <AppProgress size={20} stroke={2} indeterminate />
+      </span>
     </ComponentTag>
   )
 }
@@ -69,14 +74,15 @@ const rootStyle = (props: AppButtonProps) => {
     width: ${props.icon ? sizes[props.size].height : undefined};
     height: ${sizes[props.size].height};
     padding: ${props.icon ? 0 : sizes[props.size].padding};
-    appearance: none;
     font: inherit;
     font-size: ${sizes[props.size].fontSize};
     color: inherit;
     text-decoration: none;
+    pointer-events: ${props.loading ? 'none' : undefined};
     border: solid 1px ${props.outlined ? 'currentcolor' : 'transparent'};
     border-radius: 8px;
     outline: none;
+    appearance: none;
 
     &::before {
       position: absolute;
@@ -102,12 +108,30 @@ const rootStyle = (props: AppButtonProps) => {
   `
 }
 
-const contentStyle = css`
-  display: flex;
-  gap: 8px;
-  align-items: center;
-  justify-content: center;
-  line-height: 1;
-  user-select: none;
-  border-radius: inherit;
-`
+const contentStyle = (loading: boolean) => {
+  return css`
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    justify-content: center;
+    line-height: 1;
+    user-select: none;
+    border-radius: inherit;
+    opacity: ${loading ? 0 : 1};
+  `
+}
+
+const loaderStyle = (loading: boolean) => {
+  return css`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    pointer-events: none;
+    opacity: ${loading ? 1 : 0};
+  `
+}
