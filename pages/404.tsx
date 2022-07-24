372c1bd86d78254bef88fe674ade611d3036c88b
Hiratake <hiratake@hiratake.xyz>
2022-07-24 11:08:45 +0900

    chore: 404ページにnoindexとnofollowを付与

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,10 @@
 // pages > 404
 
 import { NextPage } from 'next'
-import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 import { css } from '@emotion/react'
 import { LCenter } from '@/components/LCenter'
 import { LStack } from '@/components/LStack'
-import { config } from '@/utils/config'
 import { rem } from '@/utils/style'
 
 // ----------------------------------------
@@ -15,15 +14,15 @@ import { rem } from '@/utils/style'
 export const NotFoundPage: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>{`ページがみつかりません｜${config.name}`}</title>
-      </Head>
+      <NextSeo title="ページがみつかりません" noindex nofollow />
 
       <LCenter andText>
         <LStack space="16px">
           <h1 css={headingStyle}>404</h1>
           <p css={textStyle}>
-            ページが見つかりませんでした。URLに間違いがないかお確かめください。
+            ページがみつかりませんでした。
+            <br />
+            お手数おかけしますが、URLに間違いがないかお確かめください。
           </p>
         </LStack>
       </LCenter>
