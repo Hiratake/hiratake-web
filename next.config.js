239467f284da7bc91683c82d1013c3b62472fad0
Hiratake <hiratake@hiratake.xyz>
2022-08-02 08:08:24 +0900

    build: webpack bundle analyze を導入

--- next.config.js
+++ next.config.js
@@ -24,4 +24,9 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false,
+})
+
+module.exports = withBundleAnalyzer(nextConfig)
