a6e6252d4b6ae853802397778687bf8eeb60bc28
Hiratake <hiratake@hiratake.xyz>
2023-06-28 11:09:54 +0900

    init commit

--- lib/client.ts
+++
@@ -1,8 +0,0 @@
-// lib > client
-
-import { createClient } from 'microcms-js-sdk'
-
-export const client = createClient({
-  serviceDomain: process.env.MICROCMS_DOMAIN || '',
-  apiKey: process.env.MICROCMS_APIKEY || '',
-})
