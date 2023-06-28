889d547e13b31d7ab35e10ae3cf07aec664fa40f
Hiratake <hiratake@hiratake.xyz>
2023-06-28 12:41:21 +0900

    ci: nuxtアプリケーションのlinter設定を追加

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  eslint: {
+    lintOnStart: false,
+  },
+  modules: ['@nuxtjs/eslint-module', '@nuxtjs/stylelint-module'],
   srcDir: 'src/',
+  stylelint: {
+    lintOnStart: false,
+  },
+  typescript: {
+    shim: false,
+    strict: true,
+  },
 })
