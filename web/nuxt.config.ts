94b6989bf84d60bbd7670a20fff3455043a40106
Hiratake <hiratake@hiratake.xyz>
2023-06-29 00:05:42 +0900

    build: nuxtアプリにtailwindcssの設定を追加

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,10 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['@/assets/tailwind.css'],
   devtools: { enabled: true },
   eslint: {
     lintOnStart: false,
   },
   modules: ['@nuxtjs/eslint-module', '@nuxtjs/stylelint-module'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   srcDir: 'src/',
   stylelint: {
     lintOnStart: false,
