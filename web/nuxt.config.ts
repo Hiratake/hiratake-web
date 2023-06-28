1e765643f75fb29297b2252208bce734d09dc835
Hiratake <hiratake@hiratake.xyz>
2023-06-29 08:40:46 +0900

    feat: seoの設定をいい感じにしてくれるモジュールを追加

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -5,11 +5,20 @@ export default defineNuxtConfig({
   eslint: {
     lintOnStart: false,
   },
+  extends: ['nuxt-seo-kit'],
   modules: ['@nuxtjs/eslint-module', '@nuxtjs/stylelint-module'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  runtimeConfig: {
+    public: {
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://hiratake.dev',
+      siteName: 'Hiratake Web',
+      siteDescription: 'Personal blog of Hiratake',
+      language: 'ja',
     },
   },
   srcDir: 'src/',
--- web/src/app.vue
+++ web/src/app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtPage />
+  <SeoKit />
+  <Body
+    :class="[
+      'bg-white text-slate-700',
+      'dark:bg-slate-900 dark:text-slate-100',
+    ]"
+  />
 </template>
