ee664824115ed7d6568155781f6defc717df2f47
Hiratake <hiratake@hiratake.xyz>
2023-06-30 11:55:02 +0900

    chore: デフォルトのレイアウトを追加

---
+++ web/src/layouts/default.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const route = useRoute()
+
+/** 現在のページがトップページかどうか */
+const isTop = computed<boolean>(() => route.path === '/')
+</script>
+
+<template>
+  <main class="mx-auto box-content max-w-3xl px-6">
+    <template v-if="isTop">
+      <slot />
+    </template>
+
+    <template v-else>
+      <article>
+        <div class="prose max-w-none dark:prose-invert">
+          <slot />
+        </div>
+      </article>
+    </template>
+  </main>
+</template>
