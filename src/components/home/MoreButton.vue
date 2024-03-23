1742429a631672796af23e6baf02ed638926fbb9
Hiratake <hiratake@hiratake.xyz>
2024-03-23 16:57:32 +0900

    refactor: もっとみるボタンを別コンポーネントに切り出し

---
+++ src/components/home/MoreButton.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+// Icons
+import { PhArrowCircleRight } from '@phosphor-icons/vue'
+// Utils
+import { withoutTrailingSlash, withTrailingSlash } from 'ufo'
+
+type HomeMoreButtonProps = {
+  /** クリック時に移動するページのURL */
+  to: string
+}
+
+const props = defineProps<HomeMoreButtonProps>()
+
+const website = useWebsite()
+
+/** 末尾のスラッシュを考慮したURL */
+const url = computed(() =>
+  website.value.site.trailingSlash
+    ? withTrailingSlash(props.to)
+    : withoutTrailingSlash(props.to),
+)
+</script>
+
+<template>
+  <NuxtLink
+    :to="url"
+    class="relative mt-0.5 flex items-center gap-2 rounded-3xl px-5 py-1.5 transition-colors before:absolute before:inset-0 before:-z-10 before:m-auto before:scale-0 before:rounded-full before:bg-primary before:transition-transform after:absolute after:inset-0 after:rounded-3xl after:border-2 after:border-current after:transition-colors hover:text-white hover:before:scale-100 hover:after:border-transparent md:px-6 md:py-2"
+  >
+    <span class="text-xs">もっとみる</span>
+    <PhArrowCircleRight class="mt-0.5 size-4" weight="bold" />
+  </NuxtLink>
+</template>
--- src/components/home/Profile.vue
+++ src/components/home/Profile.vue
@@ -1,7 +1,4 @@
 <script lang="ts" setup>
-// Icons
-import { PhArrowCircleRight } from '@phosphor-icons/vue'
-
 type HomeProfileProps = {
   /** セクションのタイトル */
   title: string
@@ -22,13 +19,7 @@ const props = defineProps<HomeProfileProps>()
       >
         {{ props.title }}
       </h2>
-      <NuxtLink
-        to="/about/"
-        class="relative mt-0.5 flex items-center gap-2 rounded-3xl px-5 py-1.5 transition-colors before:absolute before:inset-0 before:-z-10 before:m-auto before:scale-0 before:rounded-full before:bg-primary before:transition-transform after:absolute after:inset-0 after:rounded-3xl after:border-2 after:border-current after:transition-colors hover:text-white hover:before:scale-100 hover:after:border-transparent md:px-6 md:py-2"
-      >
-        <span class="text-xs">もっとみる</span>
-        <PhArrowCircleRight class="mt-0.5 size-4" weight="bold" />
-      </NuxtLink>
+      <HomeMoreButton to="/about/" />
     </div>
     <div>
       <p
