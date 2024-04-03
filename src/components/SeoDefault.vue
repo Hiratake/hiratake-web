c74901ed0471c1b3e8aa154fd6dccfff62475c45
Hiratake <hiratake@hiratake.xyz>
2024-04-03 14:11:04 +0900

    refactor: メタタグの設定の一部をuseHeadからuseSeoMetaへ移動

--- src/components/SeoDefault.vue
+++ src/components/SeoDefault.vue
@@ -1,26 +1,16 @@
 <script lang="ts" setup>
 const website = useWebsite()
 
-/** Google Tag Manager ID */
-const gtmId = website.value.gtmId
-/** ウェブサイトの名前 */
-const name = website.value.name
-/** ウェブサイトのテーマカラー */
-const themeColor = website.value.themeColor
-
 useHead({
   htmlAttrs: { prefix: 'og: https://ogp.me/ns#', dir: 'ltr' },
-  meta: [
-    { name: 'apple-mobile-web-app-title', content: name },
-    { name: 'application-name', content: name },
-    { name: 'theme-color', content: themeColor },
-    { name: 'msapplication-TileColor', content: themeColor },
-    { name: 'msapplication-Config', content: '/browserconfig.xml' },
-  ],
   link: [
     { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' },
     { rel: 'manifest', href: '/site.webmanifest' },
-    { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: themeColor },
+    {
+      rel: 'mask-icon',
+      href: '/safari-pinned-tab.svg',
+      color: website.value.themeColor,
+    },
     {
       rel: 'icon',
       type: 'image/png',
@@ -48,17 +38,22 @@ useHead({
   script: [
     {
       key: 'gtm',
-      innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${gtmId}');`,
+      innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${website.value.gtmId}');`,
     },
   ],
   noscript: [
     {
-      key: 'gtm',
-      innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+      key: 'gtm-noscript',
+      innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=${website.value.gtmId}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
     },
   ],
 })
 useSeoMeta({
+  appleMobileWebAppTitle: website.value.name,
+  applicationName: website.value.name,
+  msapplicationConfig: '/browserconfig.xml',
+  msapplicationTileColor: website.value.themeColor,
+  themeColor: website.value.themeColor,
   titleTemplate: '%pageTitle',
   twitterCard: 'summary_large_image',
   twitterSite: website.value.socials.x.handle,
