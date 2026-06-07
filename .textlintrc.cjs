0cfc4ba04f323f9a846e25844a03f5f3c11d251e
Hiratake <hiratake@hiratake.xyz>
2026-06-07 11:51:11 +0900

    ci: textlintの設定を変更 (#2306)

--- .textlintrc.cjs
+++ .textlintrc.cjs
@@ -10,20 +10,28 @@ module.exports = {
         { name: 'updated', required: true },
       ],
     },
-    // 太字の前後の半角スペースをチェック
-    // https://github.com/Hiratake/textlint-rule-space-around-bold
-    '@hiratake/textlint-rule-space-around-bold': { after: true, before: true },
     // 括弧が対になっているかチェック
     // https://github.com/textlint-rule/textlint-rule-no-unmatched-pair
-    '@textlint-rule/textlint-rule-no-unmatched-pair': true,
-    // 単語などの前後の半角スペースをチェック
-    // https://github.com/textlint-ja/textlint-rule-preset-ja-spacing
-    'ja-space-between-half-and-full-width': { space: ['alphabets', 'numbers'] },
-    'ja-space-around-code': { after: true, before: true },
-    'ja-space-around-link': { after: true, before: true },
+    '@textlint-rule/no-unmatched-pair': true,
     // 半角英字と全角英字の混在をチェック
     // https://github.com/textlint-ja/textlint-rule-no-mixed-zenkaku-and-hankaku-alphabet
     'no-mixed-zenkaku-and-hankaku-alphabet': true,
+    // スペースをチェック
+    // https://github.com/textlint-ja/textlint-rule-preset-ja-spacing
+    'preset-ja-spacing': {
+      'ja-nakaguro-or-halfwidth-space-between-katakana': true,
+      'ja-no-space-around-parentheses': false,
+      'ja-no-space-between-full-width': false,
+      'ja-space-between-half-and-full-width': {
+        space: ['alphabets', 'numbers'],
+      },
+      'ja-space-after-exclamation': true,
+      'ja-space-after-question': true,
+      'ja-space-around-code': { before: true, after: true },
+      'ja-space-around-emphasis': { before: true, after: true },
+      'ja-space-around-link': { before: true, after: true },
+      'ja-space-around-strong': { before: true, after: true },
+    },
   },
   filters: {
     // https://github.com/textlint/textlint-filter-rule-allowlist
