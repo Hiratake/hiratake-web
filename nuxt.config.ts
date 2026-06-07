cfad1ad2e978b8334fefc07c0eec1a6feead28df
Hiratake <hiratake@hiratake.xyz>
2026-06-07 12:07:58 +0900

    build(deps): 依存関係を更新 (#2307)
    * build(deps): 依存関係を更新
    
    * fix: package.jsonのpnpmバージョンを変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,4 +66,7 @@ export default defineNuxtConfig({
   },
   srcDir: 'src/',
   tailwindcss: { cssPath: '@/assets/tailwind.css' },
+  vite: {
+    optimizeDeps: { include: ['@headlessui/vue'] },
+  },
 })
